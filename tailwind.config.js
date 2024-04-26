61c316657633f29a6941b463583ed0ccd12c41cb
Dheeraj Bisht <dheerajb@sahaj.ai>
2024-04-26 10:36:53 +0530

    Add custom styles to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      nunito: ['Nunito Sans', 'sans-serif'],
+    },
+    fontWeight: {
+      normal: '300',
+      bold: '600',
+      extraBold: '800',
+    },
+    extend: {
+      colors: {
+        'dark-element': 'hsl(209, 23%, 22%)',
+        'dark-bg': 'hsl(207, 26%, 17%)',
+        'dark-text': 'hsl(0, 0%, 100%)',
+        'light-element': 'hsl(0, 0%, 100%)',
+        'light-text': 'hsl(200, 15%, 8%)',
+        'light-input': 'hsl(0, 0%, 52%)',
+        'light-bg': 'hsl(0, 0%, 98%)',
+      },
+    },
   },
   plugins: [],
 };
