352a5fabdc75eb74d1840598210f36c75994e575
Dheeraj Bisht <dheerajb@sahaj.ai>
2024-04-27 11:18:22 +0530

    Add SearchInput to Home page

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
   let { theme, changeTheme } = useContext(ThemeContext);
 
   return (
-    <div className="flex justify-between px-20 py-6 bg-light-bg text-light-text shadow-md shadow-gray-100 dark:bg-dark-element dark:text-dark-text">
+    <div className="flex justify-between px-20 py-6 bg-light-bg text-light-text shadow-md shadow-gray-100 dark:shadow-gray-800 dark:bg-dark-element dark:text-dark-text">
       <div className="font-extraBold">Where in the world?</div>
       <button className="flex items-center" onClick={changeTheme}>
         {theme === 'dark' ? <MdOutlineLightMode /> : <MdOutlineDarkMode />}
--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Navbar from '../../component/Navbar';
+import SearchInput from './component/SearchInput';
 
 function Home() {
   return (
-    <>
+    <div className="w-screen h-screen bg-light-bg dark:bg-dark-bg">
       <Navbar />
-    </>
+      <div className="px-20 pt-10 flex justify-between items-start">
+        <SearchInput />
+      </div>
+    </div>
   );
 }
 
