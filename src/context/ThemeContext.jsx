207f4651c75bebfb9224997a6227a963db8317a7
Dheeraj Bisht <dheerajb@sahaj.ai>
2024-04-27 10:41:33 +0530

    Add persistance to theme

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -3,10 +3,25 @@ import { createContext, useState } from 'react';
 const ThemeContext = createContext('light');
 
 const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState('light');
+  const preferenceTheme = localStorage.getItem('theme');
+  const systemTheme = window.matchMedia
+    ? window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark'
+      : 'light'
+    : 'dark';
+
+  if (preferenceTheme === null) {
+    localStorage.setItem('theme', systemTheme);
+  }
+
+  const [theme, setTheme] = useState(preferenceTheme || systemTheme);
+
   const changeTheme = () => {
-    setTheme(theme === 'dark' ? 'light' : 'dark');
+    const currentTheme = theme === 'dark' ? 'light' : 'dark';
+    setTheme(currentTheme);
+    localStorage.setItem('theme', currentTheme);
   };
+
   return <ThemeContext.Provider value={{ theme: theme, changeTheme: changeTheme }}>{children}</ThemeContext.Provider>;
 };
 
